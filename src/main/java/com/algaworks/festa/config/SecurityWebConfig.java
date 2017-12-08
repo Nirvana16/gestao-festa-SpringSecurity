e12a4ad27840a33458dfa5a79fd100207e0f8c3b
Nirvana16 <zedanilo161@gmail.com>
2017-12-08 10:18:49 -0200

    feita autenticação em memoria

--- src/main/java/com/algaworks/festa/config/SecurityWebConfig.java
+++ src/main/java/com/algaworks/festa/config/SecurityWebConfig.java
@@ -1,23 +1,20 @@
 package com.algaworks.festa.config;
 
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityWebConfig extends WebSecurityConfigurerAdapter {
 	
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http
-        .authorizeRequests()
-            // Para qualquer requisição (anyRequest) é preciso estar 
-            // autenticado (authenticated).
-            .anyRequest().authenticated()
-        .and()
-        .httpBasic();
-  }
-  
-  // usuario padrao: user
-  // senha padrao será exibida no console
-}
+	@Override
+	  public void configure(AuthenticationManagerBuilder builder) throws Exception {
+	    builder
+	        .inMemoryAuthentication()
+	        .withUser("Lula").password("13")
+	            .roles("USER")
+	        .and()
+        .withUser("Bolsonaro").password("51")
+	            .roles("USER");
+	  }
+ }
